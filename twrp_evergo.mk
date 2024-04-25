aa295ac87e1541a713e791895858720af99348a5
bhuvneshbaghel7820 <120732483+bhuvneshbaghel7820@users.noreply.github.com>
2024-04-25 14:22:36 +0530

    Update and rename aosp_evergo.mk to twrp_evergo.mk

--- twrp_evergo.mk
+++ twrp_evergo.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common Twrp stuff.
 $(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from evergo device
 $(call inherit-product, device/xiaomi/evergo/device.mk)
 
 PRODUCT_DEVICE := evergo
-PRODUCT_NAME := aosp_evergo
+PRODUCT_NAME := twrp_evergo
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := evergo
 PRODUCT_MANUFACTURER := xiaomi
